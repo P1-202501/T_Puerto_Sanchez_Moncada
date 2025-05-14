1b4778e0adf530f371d3abb50b1b8654d3177825
Daniel-sanchez-Angel <sanchez.daniel@uces.edu.co>
2025-05-14 13:19:18 -0500

    renombramientos

--- biometria_pacientes.py
+++ biometria_pacientes.py
@@ -1,11 +1,11 @@
 # PARTE 2
 
-# 7. Crear columna IMC = Peso / (Talla/100)^2
+# 7. columna IMC
 df['IMC'] = df['Peso'] / (df['Talla']/100)**2
 print("\nColumna IMC creada:")
 print(df[['Peso', 'Talla', 'IMC']].head())
 
-# 8. Crear clasificación de IMC
+# 8. clasificación de IMC
 def clasificar_imc(imc):
     if imc < 18.5:
         return 'Bajo peso'
@@ -20,17 +20,17 @@ df['Clasificacion_IMC'] = df['IMC'].apply(clasificar_imc)
 print("\nClasificación IMC:")
 print(df[['IMC', 'Clasificacion_IMC']].head())
 
-# 9. Crear variable binaria Sedentario
+# 9. variable binaria Sedentario
 df['Sedentario'] = df['Actividad física (min/sem)'] < 60
 print("\nColumna Sedentario creada:")
 print(df[['Actividad física (min/sem)', 'Sedentario']].head())
 
-# 10. Crear variable Hipertenso
+# 10. variable Hipertenso
 df['Hipertenso'] = (df['PAS'] >= 140) | (df['PAD'] >= 90)
 print("\nColumna Hipertenso creada:")
 print(df[['PAS', 'PAD', 'Hipertenso']].head())
 
-# 11. Crear variable Metabólicamente alterado
+# 11. variable Metabólicamente alterado
 cond1 = df['Glucosa'] > 126
 cond2 = df['Colesterol'] > 240
 cond3 = df['IMC'] > 30
